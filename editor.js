e288aae1d552eaff1d89c4f95c415d77c96a5b96
BuildTools <unconfigured@null.spigotmc.org>
2019-02-06 12:22:49 +0100

    Ajout de l'analyse basique des mots complexes.

--- editor.js
+++ editor.js
@@ -1,11 +1,266 @@
+
+/*=======================================================================
+  VARIABLES
+  =======================================================================*/
 var quill = new Quill('#editor', {
   modules: {
     toolbar: {
 	    container: '#toolbar'
-	  }
-  }
+	  },
+    imageResize: {
+      displaySize: true
+    },
+    imageDrop: true
+  },
+  theme: 'snow'
 });
+var babelnetKey = "4ac54b0c-b866-4afd-bd04-678a98cc4da0";
+var slider = document.getElementById('zoom-range');
+var page = document.getElementById('page');
+var verifyButton = document.getElementById('verify-button');
+var pdfButton = document.getElementById('pdf-button');
+var analysisContent = document.getElementById('analysis-content');
+var stanfordConnection = document.getElementById('stanford-connection');
+var lexique3Connection = document.getElementById('lexique3-connection');
+var lexique3Progress = document.getElementById('lexique3-progress');
+
+var complexWords = [];
+
+/*=======================================================================
+  QUILL EVENTS
+  =======================================================================*/
+quill.on('selection-change', onSelectionChange);
+
+/*=======================================================================
+  EVENTS
+  =======================================================================*/
+
+verifyButton.onclick = onVerifyClick;
+slider.oninput = refreshPageScale;
+
+/*=======================================================================
+  INITIALIZATION
+  =======================================================================*/
+
+refreshPageScale();
+
+/*=======================================================================
+  FUNCTIONS
+  =======================================================================*/
+
+function analyzeText(text){
+  // Clear list of complex words
+  complexWords.length = 0;
+  setLexique3Progress(0);
+
+  var request = createCORSRequest("POST", 'http://sioux.univ-paris8.fr:9000/');
+  request.onreadystatechange = async function(){
+    if(request.readyState == 4){
+      if(request.status == 200){
+        setStatus("Stanford", "ok");
+        // Parse la réponse en JSON.
+        var obj = JSON.parse(request.responseText);
+
+        // Count the total number of tokens.
+        var totalTokens = 0;
+        for(var i = 0; i < obj.sentences.length; i++){
+          totalTokens += obj.sentences[i].tokens.length;
+        }
+        var progress = 0;
+
+        // Récupère le résultat pour chaque phrase.
+        for(var i = 0; i < obj.sentences.length; i++){
+          var s = obj.sentences[i];
+          // Récupère le résultat pour chaque mot.
+          for(var t = 0; t < s.tokens.length; t++){
+            var word = {};
+            // Position de la première lettre
+            word.startOffset = s.tokens[t].characterOffsetBegin;
+            // Longueur
+            word.length = s.tokens[t].characterOffsetEnd - word.startOffset;
+            // Le mot
+            word.text = s.tokens[t].word;
+            // Sa fonction
+            word.pos = s.tokens[t].pos;
+            // Recherche plus d'informations dans le lexique si nécessaire.
+            if(needLexique3(word.pos)){
+              // Autres informations
+              let data = await checkLexique3(word);
+              // Fréquence du mot
+              word.frequency = Math.max(data.movies, data.books);
+              // Ajoute le mot à la liste des mots complexes si besoin.
+              switch(frequencyToText(word.frequency)){
+                case 'inconnu': case 'très rare': case 'rare': case 'commun':
+                complexWords.push(word); break;
+              }
+            }
+            setLexique3Progress((++progress * 100) / totalTokens);
+          }
+        }
+
+        displayAnalysisResults();
+        setTimeout(clearStatus, 1000);
+      } else {
+        setStatus("Stanford", "echec");
+      }
+    } else {
+      setStatus("Stanford", "en cours");
+    }
+  }
+  request.send(text);
+}
+
+function setLexique3Progress(value){
+  let percent = ''+Math.floor(value)+'%';
+  lexique3Progress.style.width = percent;
+  lexique3Progress.setAttribute('aria-valuenow', value);
+  lexique3Progress.innerHTML = percent;
+}
+
+function displayAnalysisResults(){
+  // Ajoute les résultats à la page d'analyse.
+  analysisContent.innerHTML = "";
+  if(complexWords.length > 0){
+    analysisContent.insertAdjacentHTML('beforeend',`<div class="alert alert-danger" role="alert">${complexWords.length} mots compliqués !</div>`);
+    for(var i = 0; i < complexWords.length; i++){
+      analysisContent.insertAdjacentHTML('beforeend',
+      `<input type='button' title='${frequencyToText(complexWords[i].frequency)}' class='btn btn-outline-danger btn-sm' type="button" value='${complexWords[i].text}'
+      onclick='quill.setSelection(${complexWords[i].startOffset}, ${complexWords[i].length});' />`);
+    }
+  } else {
+    analysisContent.insertAdjacentHTML('beforeend',`<div class="alert alert-success" role="alert">Les mots semblent simples !</div>`);
+  }
+}
+
+async function parseSentence(sentence){
+
+}
+
+function setStatus(moduleName, status){
+  var divText;
+  switch(status){
+    case 'en cours':
+    divText = `<div class="alert alert-info" role="alert">${moduleName} : En cours</div>`; break;
+    case 'echec':
+    divText = `<div class="alert alert-danger" role="alert">${moduleName} : Echec</div>`; break;
+    case 'ok':
+    divText = `<div class="alert alert-success" role="alert">${moduleName} : OK</div>`; break;
+  }
+  switch(moduleName){
+    case 'Stanford':
+    stanfordConnection.innerHTML=divText;
+    stanfordConnection.style.display = 'block';
+    break;
+    case 'Lexique3':
+    lexique3Connection.style.display = 'block';
+    break;
+  }
+}
+
+function clearStatus(){
+  console.log("Test");
+  stanfordConnection.style.display = 'none';
+  lexique3Connection.style.dipslay = 'none';
+}
+
+function needLexique3(pos){
+  switch(pos){
+    case 'PUNCT' : case 'ADP' : case 'DET' : case 'PRON' : return false;
+    default: return true;
+  }
+}
+
+async function checkLexique3(word){
+  setStatus("Lexique3", "en cours");
+  // S'assure que le mot est en minuscules.
+  let text = word.text.toLowerCase();
+  // Enlève le tiret si il y en a un au début du mot.
+  if(text.startsWith("-")) text = text.substring(1, text.length);
+  // Transforme la fonction syntaxique pour être compatible avec Lexique3.
+  pos = convertPos(word.pos, 'Lexique3');
+  // Lance la requète pour rechercher les informations pour le mot et sa fonction.
+  //let response = await fetch(`http://sioux.univ-paris8.fr/simples/lexique3.php?word=${text}&pos=${pos}`)
+  let response = await fetch(`http://localhost/lexique3.php?word=${text}&pos=${pos}`)
+  .catch(function(error) {
+    setStatus("Lexique3", "echec");
+  });
+  let data = await response.json();
+
+  //console.log(text + "(" +pos +") : " + JSON.stringify(data));
+  setStatus("Lexique3", "ok");
+  return data;
+}
+
+function frequencyToText(frequency){
+  if (typeof frequency == 'undefined') return "inconnu";
+  if(frequency < 5) return "très rare";
+  if(frequency < 10) return "rare";
+  if(frequency < 20) return "commun";
+  if(frequency < 50) return "fréquent";
+  return "très fréquent";
+}
+
+function convertPos(pos, targetFormat){
+  switch(targetFormat){
+    case 'Lexique3':
+    switch(pos){
+      case 'VERB': return 'VER';
+      case 'INTJ': return 'ONO';
+      case 'PRON': return 'PRO';
+      case 'NOUN': return 'NOM';
+      default: return pos;
+    }
+    break;
+    default: return pos;
+  }
+}
 
 function logHtmlContent(){
   console.log(quill.root.innerHTML);
+}
+
+function refreshPageScale(){
+  var scaleFunction = 'scale(' + slider.value + ')';
+  //page.css('transform', scaleFunction);
+  page.style.transform = scaleFunction;
+}
+
+function onSelectionChange(range, oldRange, source){
+  if (range) {
+   if (range.length == 0) {
+     console.log('User cursor is on', range.index);
+   } else {
+     var text = quill.getText(range.index, range.length);
+     console.log('User has highlighted', text);
+     //requestDefinition(text);
+   }
+ } else {
+   console.log('Cursor not in the editor');
+ }
+}
+
+function onVerifyClick(){
+  var selection = quill.getSelection();
+  if(selection && selection.length > 0){
+    analyzeText(quill.getText(selection.index, selection.length));
+  } else {
+    analyzeText(quill.getText());
+  }
+}
+
+function onPDFClick(){
+  var doc = new jspdf();
+}
+
+function createCORSRequest(method, url){
+  var xhr = new XMLHttpRequest();
+  if("withCredentials" in xhr){
+    xhr.open(method, url, true);
+  } else if( typeof XDomainRequest != "undefined"){
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+  } else {
+    xhr = null;
+  }
+  return xhr;
 }
--- index.php
+++ index.php
@@ -3,23 +3,35 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head lang="<?php echo $str_language; ?>" xml:lang="<?php echo $str_language; ?>">
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<title>MAMP PRO</title>
-	<link rel="stylesheet" type="text/css" href="main.css"/>
+	<title>SimpLEs</title>
 
+  <!-- === HTML to PDF ===-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js" integrity="sha384-NaWTHo/8YCBYJ59830LTz/P4aQZK1sS0SneOgAvhsIl3zBu8r9RevNg5lHCHAuQ/" crossorigin="anonymous"></script>
 
 	<!-- ====== Quill ====== -->
 	<!-- Include stylesheet -->
-	<link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+	<!--link rel="stylesheet" href="https://cdn.quilljs.com/1.3.6/quill.core.css"-->
+	<link href="quill.snow.css" rel="stylesheet">
 	<!-- Include the Quill library -->
-	<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+	<script src="quill.js"></script>
+	<!-- Include the Image Drop Module for Quill -->
+	<script src="image-drop.min.js"></script>
+	<!-- Include the Image Resize Module for Quill -->
+	<script src="image-resize.min.js"></script>
+
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
 	<!-- ====== Bootstrap ====== -->
 	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <!-- Popper -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
+  <link rel="stylesheet" type="text/css" href="main.css"/>
 </head>
 
 <body>
@@ -28,55 +40,92 @@
 		<div class="box">
 
 			<div id="header">
-				<img src="SimpLES.png" id="logo" alt="SIMPLES Logo" width="250" height="49" />
+				<img src="SimpLES.png" id="logo" alt="SIMPLES Logo" height="49" />
 			</div>
 
 			<div>
 				<div id="menu">
 				</div>
 				<!-- Create toolbar container -->
-				<div id="toolbar">
-				  <!-- Add font size dropdown -->
-				  <select class="ql-size">
-				    <option value="small">Petit</option>
-				    <!-- Note a missing, thus falsy value, is used to reset to default -->
-				    <option selected>Normal</option>
-				    <option value="large">Grand</option>
-				    <option value="huge">Très grand</option>
-				  </select>
-				  <!-- Add format buttons -->
-				  <button class="ql-bold"><span style="font-weight:bold; font-family:'Cambria'">G</span></button>
-				  <button class="ql-italic"><span style="font-style:italic; font-family:'Cambria'">I</span></button>
-				  <button class="ql-underline"><span style="text-decoration: underline; font-family:'Cambria'">U</span></button>
-				  <button class="ql-strike"><span style="text-decoration: line-through; font-family:'Cambria'">S</span></button>
-				  <!-- Add subscript and superscript buttons -->
-				  <button class="ql-script" value="sub"></button>
-				  <button class="ql-script" value="super"></button>
-				  <!-- Color button -->
-				  <select class="ql-color"></select>
-					<!-- Text Alignment buttons -->
-					<button class="ql-align"><span class="glyphicon glyphicon-align-left"></span></button>
-					<button class="ql-align" value="center"><span class="glyphicon glyphicon-align-center"></span></button>
-					<button class="ql-align" value="right"><span class="glyphicon glyphicon-align-right"></span></button>
-					<button class="ql-align" value="justiy"><span class="glyphicon glyphicon-align-justify"></span></button>
+				<div class="hbox">
+					<span id="toolbar">
+						<span class="ql-formats">
+					    <select class="ql-size">
+							  <option selected>14</option>
+					      <option value="small" selected>16</option>
+					      <option value="large">21</option>
+					      <option value="huge">32</option>
+					    </select>
+					  </span>
+					  <a class="ql-bold"><span style="font-weight:bold; font-family:'Cambria'">G</span></a>
+						<span class="ql-formats">
+						  <select class="ql-color">
+								<option value="#000" selected>Black</option>
+		    				<option value="red" >Red</option>
+						    <option value="green">Green</option>
+						    <option value="blue">Blue</option>
+							</select>
+						</span>
+						<button class="ql-align" selected><span class="glyphicon glyphicon-align-left"></span></button>
+						<button class="ql-align" value="center"><span class="glyphicon glyphicon-align-center"></span></button>
+					</span>
+					<span id="analyze">
+						<button id="pdf-button" type="button" class="toolbar-button btn btn-primary">Exporter PDF</button>
+					</span>
+					<span id="analyze">
+						<button id="verify-button" type="button" class="toolbar-button btn btn-success">Verifier</button>
+					</span>
 				</div>
 			</div>
 
-			<div id="content">
-				<div id="page-container">
-					<div id="page">
-						<!-- Create the editor container -->
-						<div id="editor">
-						  <p>Hello World!</p>
-						  <p>Some initial <strong>bold</strong> text</p>
-						  <p><br></p>
+			<div class="hbox">
+				<div id="content">
+					<div id="page-container">
+						<div id="page">
+							<!-- Create the editor container -->
+							<div id="editor">
+							  <p>D'un point de vue méthodologique, ils apportent une objectivité nécessaire à la validation scientifique en traitement automatique du langage naturel. L'information n'est plus empirique, elle est vérifiée par le corpus. Il est donc possible de s'appuyer sur des corpus (à condition qu'ils soient bien formés) pour formuler et vérifier des hypothèses scientifiques.</p>
+							</div>
 						</div>
 					</div>
 				</div>
+				<button class="hcollapsible"><div class="rotate">Analyse</div></button>
+				<div class="hcollapsible-content">
+					<div id="stanford-connection"></div>
+					<div class="alert alert-info" role="alert" id="lexique3-connection">
+						<div>Lexique3 : </div>
+						<div class="progress">
+							<div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="70"
+							aria-valuemin="0" aria-valuemax="100" id="lexique3-progress" style="width:70%">
+								70%
+							</div>
+						</div>
+					</div>
+					<div class="score">90</div>
+  				<p>Résultats de l'analyze :</p>
+					<div id="analysis-content"></div>
+					<button class="collapsible">Règles prioritaires</button>
+					<div class="collapsible-content">
+	  				<p>Résultats de l'analyze</p>
+					</div>
+					<button class="collapsible">Règles très importantes</button>
+					<div class="collapsible-content">
+	  				<p>Résultats de l'analyze</p>
+					</div>
+					<button class="collapsible">Règles importantes</button>
+					<div class="collapsible-content">
+	  				<p>Résultats de l'analyze</p>
+					</div>
+				</div>
+				<!--div id="analysis-container">
+					<h1>90</h1>
+					<p>Hello World</p>
+				</div-->
 			</div>
 
-			<div>
-				<button onclick="logHtmlContent()">s HTML</button>
+				<!--button onclick="logHtmlContent()">s HTML</button-->
+			<div class="slider-container">
+				<input type="range" step="0.1" min="0.1" max="5" value="1" class="slider" id="zoom-range"/>
 			</div>
 		</div>
 
@@ -84,6 +133,37 @@
 
 	<!-- Initialize Quill editor -->
 	<script type="text/javascript" src="editor.js"></script>
+	<script>
+		var coll = document.getElementsByClassName("hcollapsible");
+		var i;
 
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var content = this.nextElementSibling;
+		    if (content.style.display === "block") {
+		      content.style.display = "none";
+		    } else {
+		      content.style.display = "block";
+		    }
+		  });
+		}
+	</script>
+	<script>
+		var coll = document.getElementsByClassName("collapsible");
+		var i;
+
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var content = this.nextElementSibling;
+		    if (content.style.display === "block") {
+		      content.style.display = "none";
+		    } else {
+		      content.style.display = "block";
+		    }
+		  });
+		}
+	</script>
 </body>
 </html>

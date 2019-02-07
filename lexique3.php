1f3ef6b7b84c1f6e944f4853caf79e9abb2738fb
sioux46 <poitrenaud@mac.com>
2019-02-07 14:22:41 +0100

    foofoo

--- lexique3.php
+++ lexique3.php
@@ -6,12 +6,12 @@ function startsWith($haystack, $needle)
      $length = strlen($needle);
      return (substr($haystack, 0, $length) === $needle);
 }
-// coucou+++++++------++++++++
+// 
 $file = fopen("Lexique382Small.csv","r");
 $word = $_GET["word"];
 $pos = $_GET["pos"];
 $data = [];
-while(! feof($file)){
+while(! feof($file)) {
   $array = fgetcsv($file);
   if(strcmp($array[0], $word) == 0 && (startsWith($array[3], $pos) || strcmp($array[3], $pos) == 0)){
     $data = ['word' => $array[0], 'pos' => $array[3], 'lemma' => $array[2], 'movies' => $array[6], 'books' => $array[7]];

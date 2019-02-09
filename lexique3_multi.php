fc7b46526f5e3daf0223407372b75f8ea9d20fc7
sioux46 <poitrenaud@mac.com>
2019-02-09 15:17:07 +0100

    version à tester. pb de char accentués résolu (?)

--- lexique3_multi.php
+++ lexique3_multi.php
@@ -7,6 +7,7 @@ require_once("connectMySQL.php");
 $base = connect();
 
 $word = $_GET["word"];
+$word = utf8_decode($word);
 $pos = $_GET["pos"];
 
 $query = "SELECT `1_ortho` AS 'word', `4_cgram` AS 'pos', `3_lemme` AS 'lemma', `7_freqlemfilms2` AS 'movies', `8_freqlemlivres` AS 'books' FROM `Lexique3` WHERE `1_ortho` = '"
@@ -31,8 +32,10 @@ $query = "SELECT `1_ortho` AS 'word', `4_cgram` AS 'pos', `3_lemme` AS 'lemma', 
   	}
   	for($j = 0; $j < $nbColonnes; $j++){
   		$colonne = $colonnes[$j]->name;
-  	//	$json = $json . "\"".$colonne."\":\"". utf8_encode($ligne[$j])."\"";
-        $json = $json . $colonne . ":" . utf8_encode($ligne[$j]);
+  		$json = $json . "\"".utf8_encode($colonne)."\":\"". utf8_encode($ligne[$j])."\"";
+
+    //    $json = $json . utf8_encode($colonne) . ":" . utf8_encode($ligne[$j]);
+
   		if ($j != $nbColonnes-1)	$json = $json .  ",";	//condition virgule dernière colonne
   	}
   	$json = $json .  "}";
@@ -44,5 +47,13 @@ $query = "SELECT `1_ortho` AS 'word', `4_cgram` AS 'pos', `3_lemme` AS 'lemma', 
 ////////
 
 $result = jsonResult($query,$base);
-echo json_encode($result);
+echo $result;
+/*
+echo "\n";
+echo $base->error; echo "\n";
+echo json_last_error_msg (); echo "\n";
+echo utf8_encode($word); echo "\n";
+//echo utf8_décode('format\u00e9'); "\n";
+echo 'coucou';
+*/
 ?>

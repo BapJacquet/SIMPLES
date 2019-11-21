073fd0d49a9f698c372afc691a2741af962bd021
Sebastien Poitrenaud <poitrenaud@mac.com>
2019-11-22 00:15:35 +0100

    detail

--- index.js
+++ index.js
@@ -1073,7 +1073,7 @@ $("#toolbarBottomMask").hover( function () {
   // blockCmd ENTER
   $("#blockCmd").on("mouseenter", function (ev) {
     triggerPseudoMouseenter(0);
-    //$(".img-txt-widget").css("display", "none");
+    $(".img-txt-widget").css("display", "none");
   });
 
   // page LEAVE
@@ -1114,7 +1114,6 @@ $("#toolbarBottomMask").hover( function () {
       var blockTop = $(this).offset().top;
       var blockHeight = $(this).height();
       if ( mouseY > blockTop && mouseY < blockTop + blockHeight ) {
-        //$(".img-txt-widget").css("display", "none");
         if ( target.id == "page" || $(target).hasClass("editor-block") || $(target).closest(".editor-block").length == 1 )  {
           let oldBlocId = activeBlocId;
           activeBlocId = Number($(this).attr("id").split("-")[1]);

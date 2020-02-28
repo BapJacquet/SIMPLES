0207719b9267b41354915614c2ad53d9f26fee42
Baptiste <baptiste.jacquet28@gmail.com>
2020-02-28 12:36:40 +0100

    Removed Look behind.

--- analyzer.js
+++ analyzer.js
@@ -172,7 +172,7 @@ var rules = [
   {priority: 3,
     text: 'Commencez&nbsp;toujours une&nbsp;nouvelle&nbsp;phrase sur&nbsp;une&nbsp;nouvelle&nbsp;ligne.',
     test: function (data) {
-      let pattern = /(?<=[\.\?\!])[^\n\.\?\!]+[\.\?\!]?/gm;
+      let pattern = /[\.\?\!][^\n\.\?\!]+[\.\?\!]?/gm;
       let result = true;
       for (let i = 0; i < data.raw.length; i++) {
         if (!Utils.isNullOrUndefined(data.raw[i].match(pattern))) {

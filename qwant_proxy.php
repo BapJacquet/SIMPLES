d8dcff40db5af7145b00d1bbf3ebae8fb744e2ca
Baptiste <baptiste.jacquet28@gmail.com>
2020-03-26 18:08:33 +0100

    Toujours pas... Seb si tu comprends ce qui se passe hésite pas à y toucher.

--- qwant_proxy.php
+++ qwant_proxy.php
@@ -1,10 +1,21 @@
 <?php
+header ("Content-type: text/json");
+
 $q = isset($_GET['q']) ? $_GET['q'] : null;
 if(!$q) {
-  die ('Please, inform query');
+  die ('{error:2, errorText: "Please, inform query"}');
 }
 $q = rawurlencode($q);
 $count = isset($_GET['count']) ? $_GET['count'] : 10;
-header ("Content-type: text/json");
-readfile("https://api.qwant.com/api/search/images?count=".$count."&q=".$q."&t=images&safesearch=1&locale=fr_fr&uiv=4");
+
+$request = "https://api.qwant.com/api/search/images?count=".$count."&q=".$q."&t=images&safesearch=1&locale=fr_fr&uiv=4";
+try {
+  $json = file_get_contents($request);
+  if($json === FALSE) {
+    echo '{error:1, errorText: "Something went wrong while reading the remote file."}';
+  }
+  echo $json;
+} catch (Exception $e) {
+  echo $e;
+}
 ?>

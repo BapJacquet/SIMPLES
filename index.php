276b02ea7089935e2c87a39eeb05d6f949bbd4b1
Baptiste <baptiste.jacquet28@gmail.com>
2020-03-27 01:38:26 +0100

    Versioning

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
 // Nomenclature : [Années depuis 2020].[Mois].[Jour].[Nombre dans la journée]
-$version = "0.03.26.4";
+$version = "0.03.27.0";
 ?>
 <html lang="fr" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml">
 <head>

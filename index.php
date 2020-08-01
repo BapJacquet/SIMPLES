f384072e16b5f51d70dd4349d653ef4b19930aaf
Sebastien Poitrenaud <poitrenaud@mac.com>
2020-08-01 12:20:30 +0200

    version 0.08.01.0

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
 // Nomenclature : [Années depuis 2020].[Mois].[Jour].[Nombre dans la journée]
-$version = "0.07.05.2";
+$version = "0.08.01.0";
 ?>
 <html lang="fr" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml">
 <head>

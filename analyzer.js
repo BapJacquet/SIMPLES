4629b099708de93dc62f476719c5321a55a3e72e
BapJacquet <41743659+BapJacquet@users.noreply.github.com>
2021-05-19 11:58:56 +0200

    Update analyser.js with license

--- analyzer.js
+++ analyzer.js
@@ -1,3 +1,23 @@
+/*
+    This file is part of the LIREC program developped for the SIMPLES project.
+    It was developped by Baptiste Jacquet and Sébastien Poitrenaud for the
+    LUTIN-Userlab from 2018 to 2020.
+    Copyright (C) 2018  Baptiste Jacquet & Sébastien Poitrenaud
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* eslint no-unused-vars: ["warn", { "varsIgnorePattern": "analyzeAllEditorContent" }] */
 /* global $ */
 /* global CustomEvent */

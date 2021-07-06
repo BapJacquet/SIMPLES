7b910fb63c0f6b4baaca419622e3577fd340a634
Sebastien Poitrenaud <poitrenaud@mac.com>
2021-07-06 11:34:01 +0200

    $version = "1.07.06.0";

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
 // Nomenclature : [Années depuis 2020].[Mois].[Jour].[Nombre dans la journée]
-$version = "0.11.04.0";
+$version = "1.07.06.0";
 ?>
 <html lang="fr" xml:lang="fr" xmlns="http://www.w3.org/1999/xhtml">
 <head>
